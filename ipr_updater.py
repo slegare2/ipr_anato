dcc92837a00269f11605a6f964210b109cc74897
Sebastien Legare <sebastien.legare.2@gmail.com>
2017-07-03 18:27:28 +0200

    restarted ipr_updater.py after accidental rm ...

--- ipr_updater.py
+++ ipr_updater.py
@@ -6,7 +6,10 @@ for use with the Anatomizer.
 import os
 import sys
 import re
+import math
 import time
+import csv
+import shutil
 import urllib.request # Cannot use requests for FTP
 
 import lxml.html
@@ -29,7 +32,7 @@ from lxml import etree
 # of the isoforms, which I was not able to retrieve otherwise.
 # But in the end I also use the fasta file to get the ordered
 # list of UniProt entries with isoforms.
-
+# ---------------------------------------------------------------------------
 
 def reporthook(blocknum, blocksize, totalsize):
     """
@@ -48,13 +51,27 @@ def reporthook(blocknum, blocksize, totalsize):
         sys.stderr.write("read %d\n" % (readsofar,))
 
 
-def local_version(prefix, suffix):
+def convert_size(size_bytes):
+   """ 
+   Convert file size from bytes to MB, GB, etc.
+   taken on Stakoverflow by James Sapam.
+   """
+   if size_bytes == 0:
+       return "0B"
+   size_name = ("B", "KB", "MB", "GB", "TB", "PB", "EB", "ZB", "YB")
+   i = int(math.floor(math.log(size_bytes, 1024)))
+   p = math.pow(1024, i)
+   s = round(size_bytes / p, 2)
+   return "%s %s" % (s, size_name[i])
+
+
+def local_version(directory, prefix, suffix):
     """ 
     Check the version of local file with given prefix and suffix.
     An example of prefix is 'match_complete' and suffix would be 'xml.gz'.
     """
     match_versions = []
-    for dlded_file in os.listdir('downloaded-files'):
+    for dlded_file in os.listdir(directory):
         if prefix in dlded_file and suffix in dlded_file:
             # Find version as first series of int characters.
             start = 0
@@ -99,27 +116,26 @@ def online_version():
 
 def fetch_match(version):
     """ Download match_complete.xml.gz. """
-    print('Downloading file match_complete-%i.xml.gz' % version)
     urllib.request.urlretrieve('ftp://ftp.ebi.ac.uk/pub/databases/interpro/'
                                'match_complete.xml.gz', 
                                'downloaded-files/match_complete-%i.xml.gz'
                                % version, reporthook
     ) 
+    print('')
 
 
 def fetch_interpro(version):
     """ Download interpro.xml.gz. """
-    print('Downloading file interpro-%i.xml.gz' % version)
     urllib.request.urlretrieve('ftp://ftp.ebi.ac.uk/pub/databases/interpro/'
                                'interpro.xml.gz', 
                                'downloaded-files/interpro-%i.xml.gz' 
                                % version, reporthook
     )
+    print('')
 
 
 def fetch_tsv(version, date):
     """ Download uniprot-hproteome.tsv.gz. """
-    print('Downloading file uniprot-hproteome-%i-%s.tsv.gz' % (version, date))
     print('This should take about a minute.')
     queryline = ('http://www.uniprot.org/uniprot/'
                  '?query=reviewed:yes+AND+organism:9606+AND+proteome:up000005640'
@@ -132,11 +148,11 @@ def fetch_tsv(version, date):
                                'uniprot-hproteome-%i-%s.tsv.gz' 
                                % (version, date)
     )
+    print('')
 
 
 def fetch_fasta(version, date):
     """ Download uniprot-hproteome.fasta.gz. """
-    print('Downloading file uniprot-hproteome-%i-%s.fasta.gz' % (version, date))
     print('This should take about a minute.')
     queryline = ('http://www.uniprot.org/uniprot/'
                  '?query=reviewed:yes+AND+organism:9606+AND+proteome:up000005640'
@@ -147,91 +163,94 @@ def fetch_fasta(version, date):
                                'uniprot-hproteome-%i-%s.fasta.gz' 
                                % (version, date)
     )
+    print('')
+
+# ---------------------------------------------------------------------------
 
 
+# 2. Extract HGNC mapping corresponding to InterPro version.
+# ===========================================================================
 
-# 2. Extract InterPro short names and parents.
+# 3. Extract InterPro short names and parents.
 #
 # Short names are used as official name for domains
 # and parents are used to merge domains that are banched.
+# +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 
-human_only = False
-exclude_family = False
+def update_shortname(version, human_only = False, exclude_family = False):
+    """
+    Write an xml file that contains the id, short name, name, 
+    parent and type of each InterPro entry.
+    """
+    # Input files.
+    infile = 'downloaded-files/interpro-%i.xml.gz' % ipr_version
+    interpro_in = gzip.open(infile,'r').read()
+    
+    # Output files.
+    if human_only and exclude_family:
+        filename = 'ipr-tmp_shortnames-nofam-human-%i.xml.gz' % version
+    if human_only and not exclude_family:
+        filename = 'ipr-tmp_shortnames-human-%i.xml.gz' % version
+    if not human_only and exclude_family:
+        filename = 'ipr-tmp_shortnames-nofam-%i.xml.gz' % version
+    if not human_only and not exclude_family:
+        filename = 'ipr-tmp_shortnames-%i.xml.gz' % version
+    short_out = gzip.open(filename, 'wt')
+        
+    # Running options message.
+    print('Extracting information from file %s.' % infile)
+    if human_only:
+        print('Keeping only features found in "Human".')
+    if exclude_family:
+        print('Excluding entries of type "Family".')
+    if not human_only and not exclude_family:
+        print('Keeping all entries.')
+    
+    
+    interpro_root = etree.fromstring(interpro_in)
+    
+    short_out.write('<interprodb>\n')
+    
+    entries = interpro_root.findall("interpro")
+    for entry in entries:
+    
+        # Find if entry is found in Human.
+        in_human = False
+        taxons = entry.findall('taxonomy_distribution/taxon_data')
+        for taxon in taxons:
+            taxon_name = taxon.get('name')
+            if taxon_name == 'Human':
+                in_human = True
+                break
+    
+        # Find if entre=y is of type Family.
+        feature_type = entry.get('type')
+    
+    
+        if not human_only or (human_only and in_human):
+            if not exclude_family or (exclude_family and feature_type != 'Family'):
+    
+                ipr = entry.get('id')
+                shortname = entry.get('short_name')
+                feature_type = entry.get('type')
+                nameline = entry.find('name')
+                name = nameline.text
+    
+                parentline = entry.find('parent_list/rel_ref')
+                try:
+                    parent = parentline.get('ipr_ref')
+                except:
+                    parent = None
+    
+                # Escape special characters that are sometimes found in InterPro domain names (",&)
+                if '"' in name:
+                    name = re.sub(r'"','&quot;',name)
+                if '&' in name:
+                    name = re.sub(r'&','&amp;',name)
+    
+                short_out.write('<interpro id="%s" short_name="%s" name="%s" parent="%s" type="%s"/>\n' 
+                                % (ipr, shortname, name, parent, feature_type) )
+    
 
-# Choose which version of InterPro to use (must have been downloaded first
-# using fetch_files.py)
-ipr_version = 63
-print('Extracting short names from InterPro version %i.' % ipr_version)
-
-# Output files.
-if human_only and exclude_family:
-    short_out = gzip.open('ipr_shortnames-nofam-human-%i.xml.gz' % ipr_version, 'wt')
-if human_only and not exclude_family:
-    short_out = gzip.open('ipr_shortnames-human-%i.xml.gz' % ipr_version, 'wt')
-if not human_only and exclude_family:
-    short_out = gzip.open('ipr_shortnames-nofam-%i.xml.gz' % ipr_version, 'wt')
-if not human_only and not exclude_family:
-    short_out = gzip.open('ipr_shortnames-%i.xml.gz' % ipr_version, 'wt')
-
-# Input files.
-infile = 'downloaded-files/interpro-%i.xml.gz' % ipr_version
-interpro_in = gzip.open(infile,'r').read()
-
-
-# Running options message.
-print('Extracting information from file %s.' % infile)
-if human_only:
-    print('Keeping only features found in "Human".')
-if exclude_family:
-    print('Excluding entries of type "Family".')
-if not human_only and not exclude_family:
-    print('Keeping all entries.')
-
-
-interpro_root = etree.fromstring(interpro_in)
-
-short_out.write('<interprodb>\n')
-
-entries = interpro_root.findall("interpro")
-for entry in entries:
-
-    # Find if entry is found in Human.
-    in_human = False
-    taxons = entry.findall('taxonomy_distribution/taxon_data')
-    for taxon in taxons:
-        taxon_name = taxon.get('name')
-        if taxon_name == 'Human':
-            in_human = True
-            break
-
-    # Find if entre=y is of type Family.
-    feature_type = entry.get('type')
-
-
-    if not human_only or (human_only and in_human):
-        if not exclude_family or (exclude_family and feature_type != 'Family'):
-
-            ipr = entry.get('id')
-            shortname = entry.get('short_name')
-            feature_type = entry.get('type')
-            nameline = entry.find('name')
-            name = nameline.text
-
-            parentline = entry.find('parent_list/rel_ref')
-            try:
-                parent = parentline.get('ipr_ref')
-            except:
-                parent = None
-
-            # Escape special characters that are sometimes found in InterPro domain names (",&)
-            if '"' in name:
-                name = re.sub(r'"','&quot;',name)
-            if '&' in name:
-                name = re.sub(r'&','&amp;',name)
-
-            short_out.write('<interpro id="%s" short_name="%s" name="%s" parent="%s" type="%s"/>\n' 
-                            % (ipr, shortname, name, parent, feature_type) )
-
-
-short_out.write('</interprodb>\n')
-
+    short_out.write('</interprodb>\n')
+# +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
--- update_ipr.py
+++ update_ipr.py
@@ -13,21 +13,71 @@ ipr_version = online_version()
 
 today = time.strftime("%d%b%Y")
 
-match_version = local_version('match_complete-', 'xml.gz')
-if match_version < ipr_version:
-    fetch_match(ipr_version)
-
-interpro_version = local_version('interpro-', 'xml.gz')
+# Interpro
+interpro_version = local_version('downloaded-files',
+                                 'interpro-', 'xml.gz')
 if interpro_version < ipr_version:
+    print('Downloading file 1 of 4: interpro-%i.xml.gz'
+          % ipr_version)
     fetch_interpro(ipr_version)
+else:
+    print('File 1 of 4, interpro-%i.xml.gz, already downloaded.\n' % ipr_version)
 
-tsv_version = local_version('uniprot-hproteome-', 'tsv.gz')
+
+# Match
+match_version = local_version('downloaded-files',
+                              'match_complete-', 'xml.gz')
+if match_version < ipr_version:
+    print('Downloading file 2 of 4: match_complete-%i.xml.gz' % ipr_version)
+    fetch_match(ipr_version)
+else:
+    print('File 2 of 4, match_complete-%i.xml.gz, already downloaded.' % ipr_version)
+    file_byte = os.path.getsize('downloaded-files/match_complete-%i.xml.gz' % ipr_version)
+    filesize = convert_size(file_byte)
+    print("This file's size is %s. It should be at least 15 GB.\n" % filesize)
+
+
+# TSV
+tsv_version = local_version('downloaded-files',
+                            'uniprot-hproteome-', 'tsv.gz')
 if tsv_version < ipr_version:
+    print('Downloading file 3 of 4: uniprot-hproteome-%i-%s.tsv.gz' % (ipr_version, today))
     fetch_tsv(ipr_version, today)
+else:
+    print('File 3 of 4, uniprot-hproteome-%i.tsv.gz, already downloaded.\n' % ipr_version)
 
-fasta_version = local_version('uniprot-hproteome-', 'fasta.gz')
+
+# FASTA
+fasta_version = local_version('downloaded-files',
+                              'uniprot-hproteome-', 'fasta.gz')
 if fasta_version < ipr_version:
+    print('Downloading file 4 of 4: uniprot-hproteome-%i-%s.fasta.gz' % (ipr_version, today))
     fetch_fasta(ipr_version, today)
+else:
+    print('File 4 of 4, uniprot-hproteome-%i.fasta.gz, already downloaded.\n' % ipr_version)
+
 # --------------------------------
 
+print(' ---- Finished fetching files. Now writing custom files. ----\n')
 
+# ====== Write custom files ======
+
+# Mapping
+mapping_version = local_version('./', 'refs_mapping-', '.xml.gz')
+if mapping_version < ipr_version:
+    #update_mapping(ipr_version)
+
+# Short names
+shortname_version = local_version('./', 'ipr_shortnames-', '.xml.gz')
+if shortname_version < ipr_version:
+    #update_shortname(ipr_version
+
+# Matching
+matching_version = local_version('./', 'ipr_reviewed_human_match-', '.xml.gz')
+if matching_version < ipr_version:
+    #update_match(ipr_version)
+
+
+# ================================
+
+print('Update of InterPro files to version %i completed.' % ipr_version)
